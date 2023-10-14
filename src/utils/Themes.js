b803a12672daecfdf17318d7152e2506576551ec
Wassim <mohamedwassim.bacha@itbs.tn>
2023-10-14 20:23:22 +0100

    first commit

---
+++ src/utils/Themes.js
@@ -0,0 +1,37 @@
+export const darkTheme = {
+    bg:"#1C1C27",
+    bgLight: "#1C1E27",
+    primary:"#854CE6",
+    text_primary:"#F2F3F4",
+    text_secondary:"#b1b2b3",
+    card:"#171721",
+    card_light: '#191924',
+    button:"#854CE6",
+    white:"#FFFFFF",
+    black:"#000000",
+}
+
+export const lightTheme = {
+    bg:"#FFFFFF",
+    bgLight: "#f0f0f0",
+    primary:"#be1adb",
+    text_primary:"#111111",
+    text_secondary:"#48494a",
+    card:"#FFFFFF",
+    button:"#5c5b5b",
+}
+
+export const blueTheme = {
+    bg: "#003366",  // Replace with your desired darker blue background color
+    bgLight: "#003366",  // You can use the same dark blue color for the light background
+    primary: "#0074E4",
+    text_primary: "#F2F3F4",
+    text_secondary: "#b1b2b3",
+    card: "#171721",
+    card_light: "#191924",
+    button: "#4A90E2",
+    white: "#FFFFFF",
+    black: "#000000",
+};
+
+
